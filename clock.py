cbf0b7cc72666900555d9fdbd6b13d5080ec1858
maksTheProgramer <maxtantiz@gmail.com>
2019-03-24 14:23:35 +0100

    update for weekeday script and made a basic clock

---
+++ clock.py
@@ -0,0 +1,5 @@
+import datetime
+i=0
+while  i<1:
+    x = datetime.datetime.now()
+    print (x.strftime("%X"))
--- learnig_to_use_dates.py
+++ learnig_to_use_dates.py
@@ -1,10 +1,22 @@
 import datetime
 x = datetime.datetime.now()
 
-y = (x.strftime("%w"))
-if y == 1 :
+print ("now is year {} {} of {}".format((x.strftime("%Y")),(x.strftime("%d")),(x.strftime("%B"))))
+
+y = x.strftime("%w") # y is a string because (x.strftime("%w")) returns a string
+
+if y == "1" : # thats why i used  "" in the "if" constructions
+    print ("Current day: Monday")
     print ("omg i hate mondays")
-elif y == 0 :
-    print ("today is cool but be careful cuz you gotta be sharp for tomorrow")
-elif y == 6:
-    print ("ooo yeah party day today")
+elif y == "0" :
+    print("Current day: Sunday")
+    print ("today is cool but be careful cuz you gotta stay sharp for tomorrow")
+elif y == "2":
+    print("Current day: Tuesday")
+    print ("alright time to put myself together")
+elif y == "3":
+    print("Current day: Wednesday")
+    print("halfway now, keep it goin'")
+elif y == "6":
+    print("Current day: Saturday")
+    print("ooo yeah party day today")
